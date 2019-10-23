93c9b3e831a9615c7205899789a5e2cd77f5123b
Sasquash23 <agnieszka.sklorz@gmail.com>
2019-10-23 17:31:57 +0200

    Module 18.4. - first RestController - task

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -16,18 +16,22 @@ public class TaskController {
         return new ArrayList<>();
     }
 
+    @RequestMapping(method = RequestMethod.GET, value = "getTasksV2")
     public TaskDto getTask(Long taskId) {
         return new TaskDto( 1L, "Ricky", "Morthy is being send on mission");
     }
 
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
     public void deleteTask(Long taskId) {
 
     }
 
+    @RequestMapping(method = RequestMethod.PUT, value = "updateTask")
     public TaskDto updateTask(TaskDto taskDto) {
         return new TaskDto( 1L, "Ricky remember", "Morthy didn't comback from mission");
     }
 
+    @RequestMapping(method = RequestMethod.POST, value = "createTask")
     public void createTask(TaskDto taskDto) {
 
     }
