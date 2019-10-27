50b0cbdb76486d51658871775cae955e0e1673f9
Sasquash23 <agnieszka.sklorz@gmail.com>
2019-10-27 19:52:07 +0100

    Module 19.2. - Connection to db from aplication level - task

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -4,6 +4,7 @@ import com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.DbService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +28,11 @@ public class TaskController {
     @RequestMapping(method = RequestMethod.GET, value = "getTask")
     public TaskDto getTask(Long taskId) {
         return new TaskDto( 1L, "Ricky", "Morthy is being send on mission");
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getTask/id={taskId}")
+    public TaskDto getTaskById(@PathVariable Long taskId) {
+        return taskMapper.mapToTaskDto(service.getTaskById(taskId));
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
