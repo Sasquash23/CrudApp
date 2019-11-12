fe5dacb0b480dd4896d43352ca2a95359329cf59
Sasquash23 <agnieszka.sklorz@gmail.com>
2019-11-12 22:25:01 +0100

    Module 22.2. Rest Template - task

--- src/main/java/com/crud/tasks/trello/client/TrelloClient.java
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -5,14 +5,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class TrelloClient {
 
     @Value("${trello.api.endpoint.prod}")
     private String trelloApiEndpoint;
+
+    @Value("${trello.app.username}")
+    private String trelloUsername;
 
     @Value("${trello.app.key}")
     private String trelloAppKey;
@@ -23,10 +31,21 @@ public class TrelloClient {
     @Autowired
     private RestTemplate restTemplate;
 
+    private URI getURITrelloBoard() {
+        return UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/" + trelloUsername + "/boards")
+                .queryParam("key", trelloAppKey)
+                .queryParam("token", trelloToken)
+                .queryParam("fields", "name,id")
+                .queryParam("lists", "all")
+                .build().encode().toUri();
+    }
+
     public List<TrelloBoardDto> getTrelloBoards() {
 
-        TrelloBoardDto[] boardsResponse = restTemplate.getForObject(
-                trelloApiEndpoint + "/members/agnieszkasklorz/boards"
-                        + "?key=" + trelloAppKey + "&token=" + trelloToken, TrelloBoardDto[].class);
+        TrelloBoardDto[] boardsResponse = restTemplate.getForObject(getURITrelloBoard(), TrelloBoardDto[].class);
+
+        return  Optional.ofNullable(boardsResponse)
+                .map(response -> Arrays.asList(response))
+                .orElse(new ArrayList<>());
     }
 }
