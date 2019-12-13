683ddf158f4c4c7d057d0bcdd1efc122cde910d6
Sasquash23 <agnieszka.sklorz@gmail.com>
2019-12-13 23:10:57 +0100

    Module 22.4. - Test shouldReturnEmptyList() added

--- src/test/java/com/crud/tasks/trello/client/TrelloClientTest.java
+++ src/test/java/com/crud/tasks/trello/client/TrelloClientTest.java
@@ -95,4 +95,18 @@ public class TrelloClientTest {
         assertEquals("http://test.com", newCard.getShortUrl());
         assertEquals(null, trelloBadgesDto.getAttachmentsByType());
     }
+
+    @Test
+    public void shouldReturnEmptyList() throws URISyntaxException {
+
+        // Given
+        URI uri = new URI("http://test.com/members/agnieszkasklorz/boards?key=test&token=test&fields=name,id&lists=all");
+        when(restTemplate.getForObject(uri, TrelloBoardDto[].class)).thenReturn(null);
+
+        // When
+        List<TrelloBoardDto> fetchedTrelloBoards = trelloClient.getTrelloBoards();
+
+        // Then
+        assertEquals(0, fetchedTrelloBoards.size());
+    }
 }
